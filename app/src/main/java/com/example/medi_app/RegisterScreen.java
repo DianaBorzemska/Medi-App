cffd8d704b8ea648953e7c0bff0ebdf5f19711a9
Diana Borzemska <dianarulv@gmail.com>
2021-11-19 13:51:29 +0000

    Fixed minor interface issues

--- app/src/main/java/com/example/medi_app/RegisterScreen.java
+++ app/src/main/java/com/example/medi_app/RegisterScreen.java
@@ -2,13 +2,23 @@ package com.example.medi_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterScreen extends AppCompatActivity {
-
+    Button registerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
+        registerBtn=(Button) findViewById(R.id.register_btn2);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Intent send = new Intent(RegisterScreen.this, LoginScreen.class);
+                startActivity(send);   }
+        });
     }
 }
