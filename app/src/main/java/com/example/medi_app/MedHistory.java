c1febd282be3e995919980596e1f87476dc7389f
Diana Borzemska <dianarulv@gmail.com>
2021-11-25 14:51:55 +0000

    Edited Medi predict interface

--- app/src/main/java/com/example/medi_app/MedHistory.java
+++ app/src/main/java/com/example/medi_app/MedHistory.java
@@ -6,19 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MedHistory extends AppCompatActivity {
     Button continuebtn;
+    TextView age;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
         continuebtn=(Button) findViewById(R.id.continue_btn);
 
+        age = findViewById(R.id.age);
+        //get age from db
+        String agefromdb="";
+        age.setText(agefromdb);
+
         continuebtn.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
                 Intent send = new Intent(MedHistory.this, HistoryContinue.class);
                 startActivity(send);   }
+
         });
 
     }
